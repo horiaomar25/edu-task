ae9d558f530115ed765b2c9c3d591efe7fd8b8b7
horiaomar25 <horiaomar6@gmail.com>
2023-11-18 20:16:58 +0000

    more styling added to form

--- edutask/app/tasks/TaskForm.jsx
+++ edutask/app/tasks/TaskForm.jsx
@@ -1,11 +1,7 @@
-import TextField from "@mui/material/TextField";
-import Textarea from "@mui/material/TextareaAutosize";
-import { useState } from "react";
+import Button from '@mui/material/Button';
 
-const TaskForm = ({handleClose}) => {
 
-  
-
+const TaskForm = ({ handleClose }) => {
   return (
     <div
       style={{
@@ -26,22 +22,30 @@ const TaskForm = ({handleClose}) => {
       <div
         style={{
           display: "flex",
-          padding: "10px 15px",
+          padding: "5px ",
           justifyContent: "space-between",
           alignItems: "center",
-          borderBottom: "1px solid black",
         }}
       >
         <h1>Add Task</h1>
-        <button onClick={handleClose} id="close-button" style={{ background: "transparent" }}>
-         
+        <button
+          onClick={handleClose}
+          id="close-button"
+          style={{ background: "transparent", border: "none", fontSize: "30px" }}
+        >
           &times;
         </button>
       </div>
+
       <form>
         <label htmlFor="taskName">Task Name</label>
-        <TextField
-          sx={{ width: "400px", height: "50px", width: '100%'}}
+        <input
+          style={{
+            width: "400px",
+            height: "50px",
+            width: "100%",
+            fontSize: "16px",
+          }}
           label="Task name"
           variant="outlined"
           type="text"
@@ -50,32 +54,81 @@ const TaskForm = ({handleClose}) => {
           placeholder="Task Name"
         />
 
-        <label htmlFor="taskDescription" style={{display: 'block', marginTop: '1em'}}>Task Description</label>
-        <textarea style={{ width: "400px", height: "50px", width: '100%'}} rows="4" cols="50">
+        <label
+          htmlFor="taskDescription"
+          style={{ display: "block", marginTop: "1em" }}
+        >
+          Task Description
+        </label>
+        <textarea
+          style={{
+            width: "400px",
+            height: "50px",
+            width: "100%",
+            fontSize: "16px",
+          }}
+          rows="4"
+          cols="50"
+        ></textarea>
 
-</textarea>
-        
-
-        <label htmlFor="taskDueDate" style={{display: 'block', marginTop: '1em'}}>Task Due Date</label>
+        <label
+          htmlFor="taskDueDate"
+          style={{ display: "block", marginTop: "1em" }}
+        >
+          Task Due Date
+        </label>
         <input
-          style={{ width: "400px", height: "50px",width: '100%' }}
+          style={{ width: "400px", height: "50px", width: "100%" }}
           type="date"
           id="taskDueDate"
           name="taskDueDate"
           placeholder="Task Due Date"
         />
 
-        <label htmlFor="taskType" style={{display: 'block', marginTop: '1em'}}>Task Type:</label>
-        <select  style={{width: '400px', height: '50px', width: '100%'}}id="select-type" name="task_type">
-          <option value="Daily" id="daily">
+        <label
+          htmlFor="taskType"
+          style={{ display: "block", marginTop: "1em" }}
+        >
+          Task Type:
+        </label>
+        <select
+          style={{
+            width: "400px",
+            height: "40px",
+            width: "100%",
+            marginBottom: "15px",
+          }}
+          id="select-type"
+          name="task_type"
+        >
+          <option
+            style={{
+              width: "400px",
+              height: "40px",
+              width: "100%",
+              marginBottom: "15px",
+            }}
+            value="Daily"
+            id="daily"
+          >
             Daily
           </option>
-          <option value="Weekly" id="weekly">
+          <option
+            style={{
+              width: "400px",
+              height: "40px",
+              width: "100%",
+              marginBottom: "15px",
+            }}
+            value="Weekly"
+            id="weekly"
+          >
             Weekly
           </option>
         </select>
 
-        <button type="submit">Add Task</button>
+        
+        <Button sx={{float: "right"}}type="submit" variant="outlined">Add Task</Button>
       </form>
     </div>
   );
