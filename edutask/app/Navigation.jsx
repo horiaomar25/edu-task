7a89f15424476ab6ae816caf53793e0bde376794
horiaomar25 <horiaomar6@gmail.com>
2023-12-24 20:06:22 +0000

    layout for log in page

--- edutask/app/Navigation.jsx
+++ edutask/app/Navigation.jsx
@@ -79,6 +79,8 @@ const Navigation = (props) => {
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
+    
+
   return (
     <>
       <Box sx={{ display: "flex" }}>
--- edutask/app/page.js
+++ edutask/app/page.js
@@ -5,9 +5,36 @@ import Image from 'next/image';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Avatar from '../public/female-avatar.png';
+
 
 export default function Home() {
   return (
-    <div></div>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                
+              }}
+            >
+              <div style={{border: '1px solid black', padding: '200px', boxShadow: '0 4px 5px'}}>
+              <h2> EduTask</h2>
+              <Image src={Avatar} alt="Avatar" width={50} style={{ borderRadius:'50%'}} />
+              
+       <Link href="/dashboard">
+                <Button variant="contained" sx={{ backgroundColor:'black', width: '100%'}}>Login</Button>
+              </Link>
+              </div>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
 }
