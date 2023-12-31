a5fdd572536bf7bc497033c90730d629165a9533
horiaomar25 <horiaomar6@gmail.com>
2023-12-31 21:35:21 +0000

    navigation test complete

--- edutask/tests/navigation.test.js
+++ edutask/tests/navigation.test.js
@@ -20,20 +20,3 @@ test('should navigate to the dashboard page and locate Daily Tasks heading', asy
 
 
 
-// Test on the form - to open form
-test('should navigate to the form page and locate the form', async ({ page }) => {
-  // start from the home page
-  await page.goto('http://localhost:3000/tasks');
-
-  // navigate to the form 
-  await page.click('text=create a task');
-
-  // the new URL should include "/form"
-  await expect(page).toHaveURL('http://localhost:3000/form');
-
-  // Check if the heading "Daily Tasks" exists on the page
-  const formElement =  await page.locator('//form');
- 
-  // Check if the heading "Daily Tasks" is visible on the page
-    expect(await formElement.isVisible()).toBeTruthy();
-});
