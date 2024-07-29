b23d3b0ff2a27cf13d2346aa9010c8f3b8aacb31
horiaomar25 <horiaomar6@gmail.com>
2024-07-29 10:25:51 +0100

    added: update to layout

--- edutask/app/page.js
+++ edutask/app/page.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
-import homepageimage from '../public/homelogo.png';
 import Image from 'next/image';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Navigation from './Navigation';
-import dashboardshot from "../assets/dashboardshot.png"
-import taskboardshot from "../assets/taskboardshot.png"
+import dashboardshot from "../assets/dashboardshot.png";
+import taskboardshot from "../assets/taskboardshot.png";
 import AboutSection from '@/Components/AboutSection';
-import information from "../assets/information.png"
+import information from "../assets/information.png";
 
 export default function Home() {
   return (
     <>
       <Navigation />
       <h1 style={{ 
-        fontSize: "7rem", 
+        fontSize: "clamp(2rem, 5vw, 7rem)", 
         textAlign: "center", 
         fontWeight: "bold", 
-        marginTop: "0.5rem", // Adjusted margin top to reduce space
-        marginBottom: "1rem", 
+        margin: "0.5rem 0", 
         letterSpacing: "-0.02em" 
       }}>
         Edu
         <span style={{ 
-          paddingBottom: "1rem", 
+          paddingBottom: "0.5rem", 
           background: "linear-gradient(to right, #8e24aa, #673ab7)", 
           WebkitBackgroundClip: "text", 
           color: "transparent" 
@@ -33,15 +31,16 @@ export default function Home() {
           Task
         </span>
       </h1>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          gap: '20px',
-          flexWrap: 'wrap',
-          marginBottom: '2rem',
-        }}
-      >
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '20px',
+        marginBottom: '2rem',
+        '@media (min-width: 600px)': {
+          flexDirection: 'row',
+        }
+      }}>
         <Link href="/dashboard">
           <Button
             sx={{
@@ -77,7 +76,7 @@ export default function Home() {
           </Button>
         </Link>
       </div>
-      <p style={{ textAlign: "center", marginTop: "1rem" }}>
+      <p style={{ textAlign: "center", margin: "1rem 0" }}>
         Simplify task management for teaching assistants, enhancing productivity and organization for a smoother school week.
       </p>
       <Box sx={{ flexGrow: 1, padding: 2 }}>
@@ -85,135 +84,113 @@ export default function Home() {
           <Grid item xs={12} sm={6} md={4}>
             <Box sx={{ border: "2px solid black", borderRadius: "10px", width: "100%", height: "200px", position: "relative", backgroundColor: "#8e24aa" }}>
               <span style={{ border: "1px solid black", width: "100%", height: "2px", display: "block", position: "absolute", top: "10%" }}></span>
-              <h5 style={{ textAlign: "center", marginTop: "100px", color: "white" }}>Daily</h5>
+              <h5 style={{ textAlign: "center", marginTop: "50%", color: "white", transform: "translateY(-50%)" }}>Daily</h5>
             </Box>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
             <Box sx={{ border: "2px solid black", borderRadius: "10px", width: "100%", height: "200px", position: "relative", backgroundColor: "#8e24aa" }}>
               <span style={{ border: "1px solid black", width: "100%", height: "2px", display: "block", position: "absolute", top: "10%" }}></span>
-              <h5 style={{ textAlign: "center", marginTop: "100px", color: "white" }}>Weekly</h5> 
-             
+              <h5 style={{ textAlign: "center", marginTop: "50%", color: "white", transform: "translateY(-50%)" }}>Weekly</h5> 
             </Box>  
-            
-            
           </Grid>
         </Grid>
       </Box>
-
-  
-  {/** This is the About Section */}
-     
-       
-        <section style={{
-  margin: "20px", 
-  padding: "20px"
-}}>
-  <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }} style={{ maxWidth: "1200px", margin: "0 auto" }}>
-    <Grid item xs={12} sm={6} md={6} container justifyContent="center" alignItems="center">
-      <Image 
-        src={information} 
-        alt="screenshot of dashboard" 
-        width={650}
-        style={{
-          borderRadius: "8px",
-          boxShadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
-          marginBottom: { xs: "20px", md: "0" },
-        }} 
-      />
-    </Grid>
-    <Grid item xs={12} sm={6} md={6} container direction="column" justifyContent="center" alignItems="center">
-  <h2 style={{ 
-    fontSize: "2rem", 
-    fontWeight: "bold", 
-    letterSpacing: "-0.02em", 
-    marginBottom: "10px" // Add some space between the h2 and p elements
-  }}> 
-    About
-  </h2>
-  <p style={{ 
-    textAlign: "center", 
-    maxWidth: "400px", // Set a maximum width for the paragraph
-    margin: "0" // Remove default margin
-  }}> 
-    This infographic, combined with EduTask, helps teaching assistants manage tasks efficiently by outlining clear communication channels and promoting effective collaboration.
-  </p>
-</Grid>
-
-
-
-  </Grid>
-</section>
-
-
-
-  {/** This is the Dashboard Section */}
-      <section style={{
-       
-        margin: "20px", 
-        padding: "20px"
-      }}>
-
-        
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }} style={{ maxWidth: "1200px", margin: "0 auto" }}>
-          <Grid item xs={12} sm={6} md={6} container direction="column" justifyContent="center" alignItems="center">
+      <section style={{ margin: "20px", padding: "20px" }}>
+        <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} style={{ maxWidth: "1200px", margin: "0 auto" }}>
+          <Grid item xs={12} sm={6} container justifyContent="center" alignItems="center">
+            <Image 
+              src={information} 
+              alt="screenshot of dashboard" 
+              width={650}
+           
+              style={{
+                borderRadius: "8px",
+                boxShadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
+                width: '100%',
+                height: 'auto',
+                marginBottom: "20px",
+              }} 
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} container direction="column" justifyContent="center" alignItems="center">
             <h2 style={{ 
-              fontSize: "2rem", 
+              fontSize: "clamp(1.5rem, 4vw, 2rem)", 
               fontWeight: "bold", 
               letterSpacing: "-0.02em", 
-              marginBottom: "10px" // Add some space between the h2 and p elements
+              marginBottom: "10px" 
+            }}> 
+              About
+            </h2>
+            <p style={{ 
+              textAlign: "center", 
+              maxWidth: "400px", 
+              margin: "0" 
+            }}> 
+              This infographic, combined with EduTask, helps teaching assistants manage tasks efficiently by outlining clear communication channels and promoting effective collaboration.
+            </p>
+          </Grid>
+        </Grid>
+      </section>
+      <section style={{ margin: "20px", padding: "20px" }}>
+        <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} style={{ maxWidth: "1200px", margin: "0 auto" }}>
+          <Grid item xs={12} sm={6} container direction="column" justifyContent="center" alignItems="center">
+            <h2 style={{ 
+              fontSize: "clamp(1.5rem, 4vw, 2rem)", 
+              fontWeight: "bold", 
+              letterSpacing: "-0.02em", 
+              marginBottom: "10px" 
             }}> 
               Dashboard
             </h2>
-            <p style={{ textAlign:"center" }}> Use the dashboard to view tasks </p>
+            <p style={{ textAlign: "center" }}> Use the dashboard to view tasks </p>
           </Grid>
-          <Grid item xs={12} sm={6} md={6} container justifyContent="center" alignItems="center">
+          <Grid item xs={12} sm={6} container justifyContent="center" alignItems="center">
             <Image 
               src={dashboardshot} 
               alt="screenshot of dashboard" 
               width={650}
+            
               style={{
                 borderRadius: "8px",
                 boxShadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
-                marginBottom: { xs: "20px", md: "0" },
+                width: '100%',
+                height: 'auto',
+                marginBottom: "20px",
               }} 
             />
           </Grid>
         </Grid>
       </section>
-
-
-        {/** This is the Task Section */}
-      <section style={{
-  margin: "20px", 
-  padding: "20px"
-}}>
-  <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }} style={{ maxWidth: "1200px", margin: "0 auto" }}>
-    <Grid item xs={12} sm={6} md={6} container justifyContent="center" alignItems="center">
-      <Image 
-        src={taskboardshot} 
-        alt="screenshot of dashboard" 
-        width={650}
-        style={{
-          borderRadius: "8px",
-          boxShadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
-          marginBottom: { xs: "20px", md: "0" },
-        }} 
-      />
-    </Grid>
-    <Grid item xs={12} sm={6} md={6} container direction="column" justifyContent="center" alignItems="center">
-      <h2 style={{ 
-        fontSize: "2rem", 
-        fontWeight: "bold", 
-        letterSpacing: "-0.02em", 
-        marginBottom: "10px" // Add some space between the h2 and p elements
-      }}> 
-        TaskBoard
-      </h2>
-      <p style={{ textAlign:"center" }}> Use the dashboard to view tasks </p>
-    </Grid>
-  </Grid>
-</section>
-
+      <section style={{ margin: "20px", padding: "20px" }}>
+        <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} style={{ maxWidth: "1200px", margin: "0 auto" }}>
+          <Grid item xs={12} sm={6} container justifyContent="center" alignItems="center">
+            <Image 
+              src={taskboardshot} 
+              alt="screenshot of task board" 
+              width={650}
+             
+              style={{
+                borderRadius: "8px",
+                boxShadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
+                width: '100%',
+                height: 'auto',
+                marginBottom: "20px",
+              }} 
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} container direction="column" justifyContent="center" alignItems="center">
+            <h2 style={{ 
+              fontSize: "clamp(1.5rem, 4vw, 2rem)", 
+              fontWeight: "bold", 
+              letterSpacing: "-0.02em", 
+              marginBottom: "10px" 
+            }}> 
+              TaskBoard
+            </h2>
+            <p style={{ textAlign: "center" }}> Use the task board to view tasks </p>
+          </Grid>
+        </Grid>
+      </section>
     </>
   );
 }
